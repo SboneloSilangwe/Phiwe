19aa107c4176bc57cd94d31353979181764c6981
Sbonelo Silangwe <silangwesbonelo08@gmail.com>
2019-03-19 16:08:57 +0200

    First commit

--- mypackag/recursion.py
+++ mypackag/recursion.py
@@ -18,12 +18,15 @@ def fibonacci(n):
         return fibonacci(n-1)+fibonacci(n-2)
 
 
-
 def factorial(n):
     if n == 1:
         return n
+    elif n==0:
+        return 1
     else:
         return n*factorial(n-1)
+
+    '''Return n!'''
 
 def reverse(word):
     return word[::-1]
